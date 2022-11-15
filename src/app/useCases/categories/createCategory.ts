5799f0ecd305c50e8bcd56f97fd6beb5470c4427
Marcos Júnior <mjworks2020@outlook.com>
2022-11-15 15:44:41 -0300

    Updates in the createCategory useCase

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(req: Request, res: Response) {
-    const { icon, name } = req.body;
-    const category = await Category.create({ icon, name });
-    res.json(category);
+    try {
+        const { icon, name } = req.body;
+        const category = await Category.create({ icon, name });
+        res.status(201).json(category);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
