b9eef635f1252b3dfdee0704846d47ba3f832fd9
Marcos Júnior <mjworks2020@outlook.com>
2022-11-15 15:33:31 -0300

    Added use json

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ mongoose.connect('mongodb://localhost:27017')
     .then(() => {
         const app = express();
         const port = 3001;
+        app.use(express.json());
         app.use(router);
         app.listen(port, () => {
             console.log('Server runing on http://localhost:3001');
