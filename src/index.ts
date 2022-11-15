46ea5e0459e705318e9f0f72d369713b48cd1d0f
Marcos Júnior <mjworks2020@outlook.com>
2022-11-15 11:28:24 -0300

    Added mongoose configuration

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const app = express();
+        const port = 3001;
+        app.listen(port, () => {
+            console.log('Server runing on http://localhost:3001');
+        });
+        console.log('Conectado ao Mongo');
+    })
+    .catch(() => console.log('Erro ao conectar ao Mongo'));
+
+
