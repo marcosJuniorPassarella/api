919d089ac106ce7ba4fb012299bc9ec433c132aa
Marcos Júnior <mjworks2020@outlook.com>
2022-11-15 22:26:25 -0300

    Created list orders useCase

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { createProducts } from './app/useCases/products/createProducts';
 import { listProducts } from './app/useCases/products/listProducts';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
 
 export const router = Router();
 const upload = multer({
@@ -31,9 +32,7 @@ router.post('/products', upload.single('image'), createProducts);
 //Get products by category
 router.get('/categories/:categoryId/products', listProductsByCategory);
 //List orders
-router.get('/orders', (req, res) => {
-    res.send('OK');
-});
+router.get('/orders', listOrders);
 //Create order
 router.post('/orders', (req, res) => {
     res.send('OK');
