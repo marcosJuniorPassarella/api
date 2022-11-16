6aa019b197bc7c759ab4ad2a68174ce95b9e4987
Marcos Júnior <mjworks2020@outlook.com>
2022-11-15 22:19:50 -0300

    Added listProductsByCategory useCase

--- src/app/useCases/categories/listProductsByCategory.ts
+++ src/app/useCases/categories/listProductsByCategory.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
-import { Category } from '../../models/Category';
+import { Product } from '../../models/Product';
 
 export async function listProductsByCategory(req: Request, res: Response) {
     try {
-        const categories = await Category.find();
-        res.json(categories);
+        const { categoryId } = req.params;
+        const products = await Product.find().where('category').equals(categoryId);
+        res.json(products);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
